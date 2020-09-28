2276133d85df6526d3e9a246138a5bc5d58bfcda
Sumukh Jadhav <48212565+SumukhJadhav@users.noreply.github.com>
2020-09-28 13:57:19 +0530

    Create Movement.cs

---
+++ Movement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class mov : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody rb;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        if(Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        }
+    }
+}
